238170a49d0026ffe0439c24d8c235aa433502c8
sealsav <rptlsk@gmail.com>
2025-02-27 18:14:13 +0300

    Add files via upload

---
+++ js/ifauth.js
@@ -0,0 +1,12 @@
+function getCookie(name) {
+	let matches = document.cookie.match(new RegExp(
+		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+if (getCookie(login) != undefined) {
+	alert("hello");
+} else {
+	alert("good");
+}
